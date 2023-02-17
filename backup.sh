03ea89538159c105bba61db32792162efa9f54eb
tomijerenko <jerenkotomi@gmail.com>
2023-02-17 13:59:48 +0100

    directory structure

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-cp ~/.vimrc .
-cp ~/.tmux.conf .
-cp ~/.bashrc .
-cp ~/.bash_aliases .
-cp ~/.config/alacritty/alacritty.yml .
---
+++ backup_linux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+cp ~/.vimrc linux/.
+cp ~/.tmux.conf linux/.
+cp ~/.bashrc linux/.
+cp ~/.bash_aliases linux/.
+cp -r ~/.config/alacritty linux/.config/.
