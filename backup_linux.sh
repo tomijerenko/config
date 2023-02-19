e0be68e366240a6a4371332c9c3df5b962c623a6
tomijerenko <jerenkotomi@gmail.com>
2023-02-19 10:31:28 +0100

    keyboard shortcuts

--- backup_linux.sh
+++ backup_linux.sh
@@ -6,3 +6,5 @@ cp ~/.bashrc linux/.
 cp ~/.bash_aliases linux/.
 cp -r ~/.config/alacritty linux/.config/.
 cp -r ~/.config/procps linux/.config/.
+
+dconf dump / | sed -n '/\[org.gnome.settings-daemon.plugins.media-keys/,/^$/p' > linux/keyboard-shortcuts.conf
